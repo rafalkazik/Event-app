33dff24bc7a22791b53094dbdcef9008caa99e84
Rafał Kazik <kazik.raf@gmail.com>
2022-07-05 23:03:33 +0200

    create navbar & add styles initial structure

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,22 @@
+const NavBar = () => {
+  return (
+    <nav className='navbar'>
+      <div className='navbar__container'>
+        <div className='navbar__nav-logo nav-logo'>
+          <h1 className='nav-logo__logo-item'>EventApp</h1>
+        </div>
+
+        <div className='navbar__nav-menu'>
+          <a href='' className='nav-menu__item'>
+            Login
+          </a>
+          <a href='' className='nav-menu__item nav-menu__item--sign-up'>
+            Sign Up
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
